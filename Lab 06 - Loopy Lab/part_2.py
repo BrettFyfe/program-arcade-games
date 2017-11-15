f330b9a12bd8e6ae3db7858eaa887f6ca0f73d88
BrettFyfe <33127251+BrettFyfe@users.noreply.github.com>
2017-11-15 08:32:40 -0600

    complete part 2
    OS= O's, OSS is just a variable to hold a value(didn't know what to call it)

--- Lab 06 - Loopy Lab/part_2.py
+++ Lab 06 - Loopy Lab/part_2.py
@@ -1,1 +1,13 @@
-#
+#Complete part 2
+Number = int(input('n: '))
+OSRow = (int(Number)*2 * 'o')
+OSSpace= int(Number) - 1
+OSS = (int(Number) - 2)
+OSCol = (OSS * 'o')
+
+for row in range(1):
+    print(OSRow)
+    for column in range(OSS):
+        print('o' + 2*OSSpace * " " + 'o')
+        
+print(OSRow)
